141a779c93a93c7db6a763b9d0bfa30fbac1a488
Facundo Arambillet <Facundoarambillet@gmail.com>
2023-09-15 21:00:43 -0300

    ..

--- gateway-service/src/main/java/com/example/gatewayservice/config/AuthFilter.java
+++ gateway-service/src/main/java/com/example/gatewayservice/config/AuthFilter.java
@@ -1,5 +1,6 @@
 package com.example.gatewayservice.config;
 
+import com.example.gatewayservice.dto.TokenDto;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpHeaders;
@@ -10,32 +11,39 @@ import org.springframework.web.reactive.function.client.WebClient.Builder;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-//@Component
-//public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> {
-//    private Builder webClientBuilder;
-//    public AuthFilter(Builder webClientBuilder) {
-//        super(Config.class);
-//        this.webClientBuilder = webClientBuilder;
-//    }
-//
-//    @Override
-//    public GatewayFilter apply(Config config) {
-//        return ((((exchange, chain) -> {
-//            if(!exchange.getResponse().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
-//                return onError(exchange, HttpStatus.BAD_REQUEST);
-//            }
-//            String tokenHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
-//            String[] chuncks = tokenHeader.split(" ");
-//            if(chuncks.length !=2 || !chuncks[0].equals("Bearer")) {
-//                return onError(exchange, HttpStatus.BAD_REQUEST);
-//            }
-//        })))
-//    }
-//
-//    public Mono<Void> onError(ServerWebExchange exchange, HttpStatus status) {
-//        ServerHttpResponse response = exchange.getResponse();
-//        response.setStatusCode(status);
-//        return response.setComplete();
-//    }
-//    public static class Config {}
-//}
+@Component
+public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> {
+    private Builder webClientBuilder;
+    public AuthFilter(Builder webClientBuilder) {
+        super(Config.class);
+        this.webClientBuilder = webClientBuilder;
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (((exchange, chain) -> {
+            if(!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION))
+                return onError(exchange, HttpStatus.BAD_REQUEST);
+            String tokenHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+            String [] chunks = tokenHeader.split(" ");
+            if(chunks.length != 2 || !chunks[0].equals("Bearer"))
+                return onError(exchange, HttpStatus.BAD_REQUEST);
+            return webClientBuilder.build()
+                    .post()
+                    .uri("http://auth-service/auth/validate?token=" + chunks[1])
+                    .retrieve().bodyToMono(TokenDto.class)
+                    .map(t -> {
+                        t.getToken();
+                        return exchange;
+                    }).flatMap(chain::filter);
+        }));
+    }
+
+
+    public Mono<Void> onError(ServerWebExchange exchange, HttpStatus status) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(status);
+        return response.setComplete();
+    }
+    public static class Config {}
+}
